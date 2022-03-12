afdd91273bdd161944f47254abf6e62e0874761f
sacbem <sacbem7801@gmail.com>
2022-03-12 19:27:52 +0800

    init project-2

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Dell/Desktop/cvt
-# Build directory: C:/Users/Dell/Desktop/cvt/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
